08318a5c57e344da4b33195f7d541a29f41df11d
Weedverine <103604958+ContraInfinito@users.noreply.github.com>
2023-10-13 11:58:31 -0600

    Update 1.pl
    GFOGOGOGGOGO

--- Prologogog/1.pl
+++ Prologogog/1.pl
@@ -63,3 +63,51 @@ sub_cadenas(_, [], []).
 ?- sub_cadenas("la", ["la casa, "el perro", "pintando la cerca"],Filtradas).
 True
 Filtradas = ["la casa, "pintando la cerca"]
+
+% Define individuals with their characteristics
+person(john, smith, [1, 1, 0, 0, 1, 0, 0, 1, 1, 0]).
+person(anna, gomez, [0, 1, 1, 0, 0, 1, 0, 1, 0, 1]).
+person(joseph, rodriguez, [1, 0, 1, 0, 1, 1, 0, 0, 1, 1]).
+person(mary, lopez, [0, 0, 1, 1, 1, 0, 0, 1, 0, 1]).
+person(louis, fernandez, [1, 0, 0, 0, 1, 1, 0, 1, 0, 1]).
+person(anna, martinez, [0, 1, 0, 0, 1, 1, 0, 1, 1, 0]).
+person(john, soto, [1, 0, 1, 1, 0, 0, 1, 0, 1, 0]).
+person(david, castillo, [0, 1, 1, 0, 1, 0, 1, 0, 0, 1]).
+person(sophia, garcia, [1, 1, 0, 0, 0, 1, 1, 0, 1, 0]).
+person(philip, ramirez, [0, 0, 1, 1, 0, 1, 1, 0, 0, 1]).
+
+% Define connections between individuals
+connections(philip, anna, 3).
+connections(anna, david, 2).
+connections(philip, john).
+connections(john, david, 4).
+
+% Define the predicate for finding the shortest route between two individuals
+shortest_route(X, Y, (Route, Weight)) :-
+    shortest_route_aux(X, Y, [X], Route, 0, Weight).
+
+shortest_route_aux(X, X, Route, Route, Weight, Weight).
+shortest_route_aux(X, Y, Visited, Route, CurrentWeight, Weight) :-
+    connections(X, Z, P1),
+    \+ member(Z, Visited),
+    NewWeight is CurrentWeight + P1,
+    shortest_route_aux(Z, Y, [Z | Visited], Route, NewWeight, Weight).
+
+% Define a sample chromosome to compare with
+sample([1, 0, 1, 0, 0, 1, 1, 1, 0, 1]).
+
+% Calculate the similarity between two chromosomes using Hamming distance
+chromosome_similarity(C1, C2, Similarity) :-
+    hamming_distance(C1, C2, Distance),
+    Similarity is (100 - Distance * 10).
+
+hamming_distance([], [], 0).
+hamming_distance([X|Xs], [Y|Ys], Distance) :-
+    hamming_distance(Xs, Ys, RestDistance),
+    Distance is RestDistance + abs(X - Y).
+
+% Find the most similar person to the given sample chromosome
+find_similar_person(Person, Percentage) :-
+    sample(Sample),
+    findall((Name, Similarity), (person(Name, _, Chromosomes), chromosome_similarity(Chromosomes, Sample, Similarity)), SimilarityList),
+    max_member((Person, Percentage), SimilarityList).
